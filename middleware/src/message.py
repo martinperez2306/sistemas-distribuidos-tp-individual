6ff142535b3f264a2cf52783e550dd19561883f5
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-24 16:02:07 -0300

    Add to string message

--- middleware/src/message.py
+++ middleware/src/message.py
@@ -1,6 +1,11 @@
 class Message:
-    def __init__(self, id, request_id, client_id, operation_id):
+    def __init__(self, id, request_id, client_id, operation_id, body):
         self.id = id
         self.request_id = request_id
         self.client_id = client_id
         self.operation_id = operation_id
+        self.body = body
+
+    def to_string(self) -> str:
+        return "MESSAGE_ID[{}]REQUEST_ID[{}]CLIENT_ID[{}]OPERATION_ID[{}]BODY[{}]"\
+            .format(self.id, self.request_id, self.client_id, self.operation_id, self.body)
