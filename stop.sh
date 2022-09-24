84c905e3a6d39507c69c959159a958119c9db47e
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-24 16:00:54 -0300

    Add stop for client

--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker compose -f docker-compose-client.yaml stop
+docker compose -f docker-compose-client.yaml down
 docker compose -f docker-compose-system.yaml stop
 docker compose -f docker-compose-system.yaml down
