dc7e0aa5ccc73f17da5fb4f3de1b0d0f9e4aae23
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-24 16:53:09 -0300

    Define 4 client methods : Start Process, Process Data, End process, Get Resutls

--- base_images/middleware_client/middleware_client.py
+++ base_images/middleware_client/middleware_client.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import constants
 import logging
+from message import Message
 import uuid
 import pika
 
@@ -7,6 +9,7 @@ class MiddlewareClient:
     def __init__(self, host, middleware_queue_id):
         self.host = host
         self.middleware_queue_id = middleware_queue_id
+        self.client_id = 1
         self.connection = None
         self.channel = None
         self.callback_queue = None
@@ -22,17 +25,33 @@ class MiddlewareClient:
 
         self.channel.basic_consume(
             queue=self.callback_queue,
-            on_message_callback=self.on_response,
+            on_message_callback=self.__on_response,
             auto_ack=True)
 
         self.response = None
         self.corr_id = None
 
-    def on_response(self, ch, method, props, body):
+    def __on_response(self, ch, method, props, body):
         if self.corr_id == props.correlation_id:
             self.response = body
 
-    def request(self, message):
+    def call_start_data_process(self):
+        request = Message(constants.CLIENT_MESSAGE_ID, 0, self.client_id, constants.START_PROCESS_ID, "")
+        return self.__request(request)
+
+    def call_process_data(self, request_id, data):
+        request = Message(constants.CLIENT_MESSAGE_ID, request_id, self.client_id, constants.PROCESS_DATA_ID, data)
+        return self.__request(request)
+
+    def call_end_data_process(self, request_id):
+        request = Message(constants.CLIENT_MESSAGE_ID, request_id, self.client_id, constants.END_PROCESS_ID, "")
+        return self.__request(request)
+
+    def call_get_results(self, request_id):
+        request = Message(constants.CLIENT_MESSAGE_ID, request_id, self.client_id, constants.GET_RESULTS_ID, "")
+        return self.__request(request)
+
+    def __request(self, message):
         logging.info("Send message: {}".format(message))
         self.response = None
         self.corr_id = str(uuid.uuid4())
