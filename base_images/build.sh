dc626f49d21099c3c2f492ac03f2eb892315bfd4
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-23 23:25:17 -0300

    Build base script

--- base_images/build.sh
+++ base_images/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build -f python-rabbitmq.dockerfile -t python-rabbitmq .
+docker build -f middleware_client/middleware_client.dockerfile -t middleware_client ./middleware_client/
