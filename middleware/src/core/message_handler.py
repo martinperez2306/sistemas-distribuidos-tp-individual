a895415b27151baa147374ab5055569da1e3d536
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-24 20:22:26 -0300

    Parse operation ID as Int'

--- middleware/src/core/message_handler.py
+++ middleware/src/core/message_handler.py
@@ -18,16 +18,23 @@ class MessageHandler:
 
     def handle_message(self, ch, method, props, body):
         logging.info("Handling client message {}".format(body))
-        message_parsed = self.__parse_message(body)
+        message_parsed = self.__parse_message(str(body))
         self.__process_message(ch, method, props, message_parsed)
 
-    def __parse_message(self, request: str) -> Message:
-        message_id = re.search(MESSAGE_ID_REGEX,request).group(1)
-        request_id = re.search(MESSAGE_REQUEST_ID_REGEX,request).group(1)
-        client_id = re.search(MESSAGE_CLIENT_ID_REGEX,request).group(1)
-        operation_id = re.search(MESSAGE_OPERATION_ID_REGEX,request).group(1)
-        body = re.search(MESSAGE_BODY_REGEX,request).group(1)
-        return Message(message_id, request_id, client_id, operation_id, body)
+    def __parse_message(self, body: str) -> Message:
+        message_id = re.search(MESSAGE_ID_REGEX,body).group(1)
+        request_id = re.search(MESSAGE_REQUEST_ID_REGEX,body).group(1)
+        client_id = re.search(MESSAGE_CLIENT_ID_REGEX,body).group(1)
+        operation_id = -1
+        try:
+            op_id = int(re.search(MESSAGE_OPERATION_ID_REGEX,body).group(1))
+            operation_id = op_id
+        except ValueError:
+            pass
+        body = re.search(MESSAGE_BODY_REGEX,body).group(1)
+        message = Message(message_id, request_id, client_id, operation_id, body)
+        logging.info("Message: {}".format(message.to_string()))
+        return message
     
     def __process_message(self, ch, method, props, message: Message):
         if (message.operation_id == START_PROCESS_ID):
