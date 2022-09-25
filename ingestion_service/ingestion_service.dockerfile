a33f0d6308c312ceefca9936cc28c563808454aa
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-25 17:03:17 -0300

    Add ingestion service App using Midd Sys Client

---
+++ ingestion_service/ingestion_service.dockerfile
@@ -0,0 +1,8 @@
+FROM middlewaresys_client
+
+# Install Ingestion Service
+RUN mkdir -p /root/ingestion_service
+WORKDIR /root/ingestion_service
+COPY . .
+RUN mv /root/middlewaresys_client /root/ingestion_service/middlewaresys_client
+ENTRYPOINT ["python3", "/root/ingestion_service/main.py"]
---
+++ ingestion_service/src/core/ingestion_service.py
@@ -0,0 +1,24 @@
+import logging
+import pika
+
+INGESTION_QUEUE_NAME = "ingestion_service_queue"
+
+class IngestionService:
+    def __init__(self):
+        self.connection = None
+        self.channel = None
+
+    def run(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=INGESTION_QUEUE_NAME, durable=True)
+
+        def handle_message(ch, method, properties, body):
+            logging.info("Received {}".format(body))
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=INGESTION_QUEUE_NAME, on_message_callback=handle_message)
+
+        logging.info('Waiting for messages. To exit press CTRL+C')
+        self.channel.start_consuming()
