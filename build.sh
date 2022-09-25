ff092ec13842462cedd4856475735cd48cdb4edf
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-25 14:57:38 -0300

    Move build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+cd base_images
 docker build -f python-rabbitmq.dockerfile -t python-rabbitmq .
 docker build -f middleware_client/middleware_client.dockerfile -t middleware_client ./middleware_client/
