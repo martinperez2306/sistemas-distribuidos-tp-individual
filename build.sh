d6a744cd69a60748412ccb8c40272d0920e8bda7
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-25 17:02:21 -0300

    Add Midd Sys Cli to build script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 cd base_images
 docker build -f python-rabbitmq.dockerfile -t python-rabbitmq .
 docker build -f middleware_client/middleware_client.dockerfile -t middleware_client ./middleware_client/
+docker build -f middleware_client/middlewaresys_client.dockerfile -t middlewaresys_client ./middlewaresys_client/
