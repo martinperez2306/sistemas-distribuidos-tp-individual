a71c14eb0bbd6d955aa3f4f1ba736e8dbf061a7a
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-26 00:21:35 -0300

    Add base service in middleware

---
+++ middleware/src/core/base_service.py
@@ -0,0 +1,25 @@
+import pika
+from .constants import *
+
+class BaseService:
+    def __init__(self, publish_queue_name):
+        self.connection = None
+        self.channel = None
+        self.publish_queue_name = publish_queue_name
+    
+    def run(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.publish_queue_name, durable=True)
+
+    def publish_data(self, data: str):
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.publish_queue_name,
+            body=str(data),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
+            ))
+
+    def shutdown(self):
+        self.connection.close()
