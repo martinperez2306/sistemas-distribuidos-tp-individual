7dcf39b47e3cd676987ece1a8089c8d0bec80be4
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-26 01:15:57 -0300

    Better naming and import in ingestion service

--- ingestion_service/src/core/ingestion_service.py
+++ ingestion_service/src/core/ingestion_service.py
@@ -2,7 +2,6 @@ import logging
 import pika
 
 from middlewaresys_client.src.core.middlewaresys_client import MiddlewareSystemClient
-from middlewaresys_client.src.core.message import Message
 from middlewaresys_client.src.core.constants import *
 #Dev import
 #from ....base_images.middlewaresys_client.src.core.middlewaresys_client import MiddlewareSystemClient
@@ -11,7 +10,7 @@ from middlewaresys_client.src.core.constants import *
 
 RABBITMQ_HOST = "rabbitmq"
 MIDDLEWARE_QUEUE = "middleware"
-INGESTION_QUEUE = "ingestion_service_queue"
+INGESTION_SERVICE_QUEUE = "ingestion_service_queue"
 INGESTION_SERVICE_ID="ingestion_service"##TODO: Obtener de configuracion
 
 class IngestionService:
@@ -24,7 +23,7 @@ class IngestionService:
         self.middleware_system_client.connect()
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=INGESTION_QUEUE, durable=True)
+        self.channel.queue_declare(queue=INGESTION_SERVICE_QUEUE, durable=True)
 
         def handle_message(ch, method, properties, body):
             logging.info("Received {}".format(body))
@@ -33,7 +32,7 @@ class IngestionService:
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=INGESTION_QUEUE, on_message_callback=handle_message)
+        self.channel.basic_consume(queue=INGESTION_SERVICE_QUEUE, on_message_callback=handle_message)
 
         logging.info('Waiting for messages. To exit press CTRL+C')
         self.channel.start_consuming()
