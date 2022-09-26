3ec3826a04c7dc9ff94755f64ab7e9c42821da23
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-25 21:43:39 -0300

    Connect to Middle and send data to filter

--- ingestion_service/src/core/ingestion_service.py
+++ ingestion_service/src/core/ingestion_service.py
@@ -1,25 +1,42 @@
 import logging
 import pika
 
+from middlewaresys_client.src.core.middlewaresys_client import MiddlewareSystemClient
+from middlewaresys_client.src.core.message import Message
+from middlewaresys_client.src.core.constants import *
+#Dev import
+#from ....base_images.middlewaresys_client.src.core.middlewaresys_client import MiddlewareSystemClient, Message
+#from ....base_images.middlewaresys_client.src.core.message import Message
+#from ....base_images.middlewaresys_client.src.core.constants import *
+
 RABBITMQ_HOST = "rabbitmq"
-INGESTION_QUEUE_NAME = "ingestion_service_queue"
+MIDDLEWARE_QUEUE = "middleware"
+INGESTION_QUEUE = "ingestion_service_queue"
+INGESTION_SERVICE_ID="ingestion_service"##TODO: Obtener de configuracion
 
 class IngestionService:
     def __init__(self):
         self.connection = None
         self.channel = None
+        self.middleware_system_client = MiddlewareSystemClient(RABBITMQ_HOST, MIDDLEWARE_QUEUE, INGESTION_SERVICE_ID)
 
     def run(self):
+        self.middleware_system_client.connect()
         self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=INGESTION_QUEUE_NAME, durable=True)
+        self.channel.queue_declare(queue=INGESTION_QUEUE, durable=True)
 
         def handle_message(ch, method, properties, body):
             logging.info("Received {}".format(body))
+            ingestion_message = self.middleware_system_client.parse_message(str(body))
+            message = Message(SERVICE_MESSAGE_ID, ingestion_message.request_id, INGESTION_SERVICE_ID, LIKE_FILTER_ID, ingestion_message.body)
+            self.middleware_system_client.call_filter_by_likes(message)
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
         self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=INGESTION_QUEUE_NAME, on_message_callback=handle_message)
+        self.channel.basic_consume(queue=INGESTION_QUEUE, on_message_callback=handle_message)
 
         logging.info('Waiting for messages. To exit press CTRL+C')
         self.channel.start_consuming()
+    
+    
