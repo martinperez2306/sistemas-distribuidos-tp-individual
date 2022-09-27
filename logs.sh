190c7eab893d44356c2fdd437e399ac2880b9ba8
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-27 13:49:50 -0300

    Add environment and logs for funny app

--- logs.sh
+++ logs.sh
@@ -3,4 +3,5 @@ docker logs middleware >& logs/middleware.log
 docker logs client >& logs/clieng.log
 docker logs ingestion_service >& logs/ingestion_service.log
 docker logs like_filter >& logs/like_filter.log
+docker logs funny_filter >& logs/funny_filter.log
 docker logs reporting_service >& logs/reporting_service.log
