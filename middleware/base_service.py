16ab244c0dbc94390da8430ffcc2efabbc46a627
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-28 03:36:06 -0300

    Add method to call propagate message in app system

--- middleware/base_service.py
+++ middleware/base_service.py
@@ -1,5 +1,6 @@
 import pika
 from dependencies.commons.constants import *
+from dependencies.commons.message import Message
 from middleware.constants import *
 
 class BaseService:
@@ -21,6 +22,10 @@ class BaseService:
             properties=pika.BasicProperties(
                 delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE
             ))
+
+    def propagate_message(self, message: Message):
+        propagate_message = Message(MIDDLEWARE_MESSAGE_ID, message.request_id, message.client_id, message.operation_id, message.body)
+        self.publish_data(propagate_message.to_string()) 
 
     def shutdown(self):
         self.connection.close()
