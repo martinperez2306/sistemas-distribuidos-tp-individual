83fe5cedbacac8aeb82386e1340d83bf12102fb5
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-30 03:10:30 -0300

    Add Results Model

---
+++ reporting_service/results.py
@@ -0,0 +1,15 @@
+from dependencies.commons.video import Video
+
+class Results:
+    def __init__(self, filtered_videos: 'list[Video]', most_viewed_day: str):
+        self.filtered_videos = filtered_videos
+        self.most_viewed_day = most_viewed_day
+
+    def __str__(self):
+        videos_str = str(self.filtered_videos)
+        most_viewed_day_str = str(self.most_viewed_day)
+        return "FILTERED_VIDEOS[{}]MOST_VIEWED_DAY[{}]"\
+            .format(videos_str, most_viewed_day_str)
+
+    def __repr__(self):
+       return self.__str__()
