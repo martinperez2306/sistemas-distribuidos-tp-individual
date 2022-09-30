762d4918210b0db92a674d9f8827b142cd4f00ee
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-09-30 03:10:05 -0300

    Video list to str

--- dependencies/commons/video.py
+++ dependencies/commons/video.py
@@ -35,8 +35,10 @@ class Video:
         self.description = matches[15]
 
     def __str__(self):
-        return "ID[{}] TITLE[{}] PUBLISHED_AT[{}] CHANNEL_ID[{}] CHANNEL_TITLE[{}] CATEGORY_ID [{}] TRENDING_DATE[{}] TAGS [{}] VIEW_COUNT [{}] \
-                LIKE[{}] DISLIKES[{}] COMMENT_COUNT[{}] THUMBNAIL_LINK[{}] COMMNETS_DISABLED[{}] RATINGS_DISABLED[{}] DESCRIPTION[{}]"\
+        return "ID[{}] TITLE[{}] PUBLISHED_AT[{}] CHANNEL_ID[{}] CHANNEL_TITLE[{}] CATEGORY_ID [{}] TRENDING_DATE[{}] TAGS [{}] VIEW_COUNT [{}] LIKE[{}] DISLIKES[{}] COMMENT_COUNT[{}] THUMBNAIL_LINK[{}] COMMNETS_DISABLED[{}] RATINGS_DISABLED[{}] DESCRIPTION[{}]"\
                     .format(self.id, self.title, self.published_at, self.channel_id, self.channel_title, self.category_id, \
                         self.trending_date, self.tags, str(self.view_count), str(self.likes), str(self.dislikes), str(self.comment_count), \
                             self.thumbnail_link, self.comments_disabled, self.ratings_disabled, self.description)
+
+    def __repr__(self):
+       return self.__str__()
