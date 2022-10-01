0eb02cb974514eb93678705f5ffbd9b8eccf6ae0
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-01 15:56:59 -0300

    Add video results with only ID TITLE AND CATEGORY ID

---
+++ reporting_service/video_result.py
@@ -0,0 +1,12 @@
+class VideoResult:
+    def __init__(self, id, title, category):
+        self.id = id
+        self.title = title
+        self.category = category
+
+    def __str__(self):
+        return "ID[{}] TITLE [{}] CATGORY[{}]"\
+            .format(self.id, self.title, self.category)
+
+    def __repr__(self):
+       return self.__str__()
