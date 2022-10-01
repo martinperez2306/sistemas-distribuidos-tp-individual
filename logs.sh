00d4a23cdb403bef46afe66de9dc49cd558e1f15
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-01 14:23:36 -0300

    Add max in compose and logs

--- logs.sh
+++ logs.sh
@@ -6,3 +6,4 @@ docker logs like_filter >& logs/like_filter.log
 docker logs funny_filter >& logs/funny_filter.log
 docker logs reporting_service >& logs/reporting_service.log
 docker logs day_grouper >& logs/day_grouper.log
+docker logs max >& logs/max.log
