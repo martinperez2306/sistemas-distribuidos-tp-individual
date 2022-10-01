ed0312d7da68eb2235f53d330e5ff4a7bf5c1214
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-01 18:57:41 -0300

    Reorder logs

--- logs.sh
+++ logs.sh
@@ -4,6 +4,6 @@ docker logs client >& logs/clieng.log
 docker logs ingestion_service >& logs/ingestion_service.log
 docker logs like_filter >& logs/like_filter.log
 docker logs funny_filter >& logs/funny_filter.log
-docker logs reporting_service >& logs/reporting_service.log
 docker logs day_grouper >& logs/day_grouper.log
 docker logs max >& logs/max.log
+docker logs reporting_service >& logs/reporting_service.log
