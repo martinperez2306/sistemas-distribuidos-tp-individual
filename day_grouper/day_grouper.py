eccd9bd005027752a548e78e2b6e84d863e8b44a
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-02 17:39:57 -0300

    Use work and routing service in ingestion and day grouper

--- day_grouper/day_grouper.py
+++ day_grouper/day_grouper.py
@@ -3,47 +3,29 @@ import pika
 
 from dependencies.commons.constants import *
 from dependencies.commons.message import Message
+from dependencies.commons.routing_serivce import RoutingService
 from dependencies.commons.utils import json_to_video
 from dependencies.middlewaresys_client.middlewaresys_client import MiddlewareSystemClient
 
 DAY_GROUPER_ID="day_grouper_1"##TODO: Obtener de configuracion
-DATE_FORMAT='%Y-%m-%dT%H:%M:%SZ'
 
-class DayGrouper:
+class DayGrouper(RoutingService):
     def __init__(self):
-        self.connection = None
-        self.channel = None
-        self.middleware_system_client = MiddlewareSystemClient(RABBITMQ_HOST, MIDDLEWARE_QUEUE, DAY_GROUPER_ID)
-        self.group_id = DAY_GROUPER_ID.split("_")[2]
+        group_id = DAY_GROUPER_ID.split("_")[2]
+        RoutingService.__init__(self, group_id, DAY_GROUPER_EXCHANGE)
         self.days_grouped = dict()
 
-    def run(self):
-        self.middleware_system_client.connect()
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=DAY_GROUPER_EXCHANGE, exchange_type='direct')
-        result = self.channel.queue_declare(queue='', exclusive=True)#TODO: Tal vez definir yo la queue?
-        queue_name = result.method.queue
-        self.channel.queue_bind(exchange=DAY_GROUPER_EXCHANGE, queue=queue_name, routing_key=self.group_id)
-
-        def handle_message(ch, method, properties, body):
-            logging.info("Received {}".format(body))
-            grouping_message = self.middleware_system_client.parse_message(body)
-            if PROCESS_DATA_OP_ID == grouping_message.operation_id:
-                self.__save_by_group(grouping_message)
-            elif END_PROCESS_OP_ID == grouping_message.operation_id:#TODO: Esto lo deberia hacer el max!
-                max_message: Message = Message(grouping_message.id, grouping_message.request_id, 
-                                                    grouping_message.source_id, grouping_message.operation_id,
-                                                    grouping_message.destination_id, str(self.days_grouped))
-                self.__propagate_message(ch, method, properties, body, max_message)
-            else:
-                self.__propagate_message(ch, method, properties, body, grouping_message)
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-        self.channel.basic_consume(queue=queue_name, on_message_callback=handle_message)
-
-        logging.info('Waiting for messages. To exit press CTRL+C')
-        self.channel.start_consuming()
+    def work(self, ch, method, properties, body):
+        grouping_message = self.middleware_system_client.parse_message(body)
+        if PROCESS_DATA_OP_ID == grouping_message.operation_id:
+            self.__save_by_group(grouping_message)
+        elif END_PROCESS_OP_ID == grouping_message.operation_id:
+            max_message: Message = Message(grouping_message.id, grouping_message.request_id, 
+                                                grouping_message.source_id, grouping_message.operation_id,
+                                                grouping_message.destination_id, str(self.days_grouped))
+            self.__propagate_message(ch, method, properties, body, max_message)
+        else:
+            self.__propagate_message(ch, method, properties, body, grouping_message)
 
     def __save_by_group(self, gruping_message: Message):
         video = json_to_video(gruping_message.body)
--- ingestion_service/ingestion_service.py
+++ ingestion_service/ingestion_service.py
@@ -1,36 +1,15 @@
-import logging
-import pika
-
 from dependencies.commons.constants import *
+from dependencies.commons.work_service import WorkService
 from dependencies.middlewaresys_client.middlewaresys_client import MiddlewareSystemClient
 
-RABBITMQ_HOST = "rabbitmq"
-MIDDLEWARE_QUEUE = "middleware"
-INGESTION_SERVICE_QUEUE = "ingestion_service_queue"
 INGESTION_SERVICE_ID="ingestion_service"##TODO: Obtener de configuracion
 
-class IngestionService:
+class IngestionService(WorkService):
     def __init__(self):
-        self.connection = None
-        self.channel = None
-        self.middleware_system_client = MiddlewareSystemClient(RABBITMQ_HOST, MIDDLEWARE_QUEUE, INGESTION_SERVICE_ID)
+        WorkService.__init__(self, INGESTION_SERVICE_ID, INGESTION_SERVICE_QUEUE)
 
-    def run(self):
-        self.middleware_system_client.connect()
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=INGESTION_SERVICE_QUEUE, durable=True)
-
-        def handle_message(ch, method, properties, body):
-            logging.debug("Received {}".format(body))
-            ingestion_message = self.middleware_system_client.parse_message(body)
-            self.middleware_system_client.call_filter_by_likes(ingestion_message)
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-        self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=INGESTION_SERVICE_QUEUE, on_message_callback=handle_message)
-
-        logging.info('Waiting for messages. To exit press CTRL+C')
-        self.channel.start_consuming()
+    def work(self, ch, method, properties, body):
+        ingestion_message = self.middleware_system_client.parse_message(body)
+        self.middleware_system_client.call_filter_by_likes(ingestion_message)
     
     
