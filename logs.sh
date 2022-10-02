5ecebcc681a87e660fc4e199463991782e633ab8
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-02 17:04:10 -0300

    Add log for instances

--- logs.sh
+++ logs.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-docker logs middleware >& logs/middleware.log
-docker logs client >& logs/clieng.log
-docker logs ingestion_service >& logs/ingestion_service.log
-docker logs like_filter >& logs/like_filter.log
-docker logs funny_filter >& logs/funny_filter.log
-docker logs day_grouper >& logs/day_grouper.log
-docker logs max >& logs/max.log
-docker logs reporting_service >& logs/reporting_service.log
+usage="Debe ingresar el numero de instancias"
+
+if [ $# -eq 0 ]; then
+  echo "$usage"
+  exit 1    
+fi
+
+INSTANCES=$1
+LOGS_DIR="logs"
+
+docker logs middleware >& "$LOGS_DIR/middleware.log"
+
+for i in $( seq 1 $INSTANCES )
+do
+    docker logs "ingestion_service_$i" >& "$LOGS_DIR/ingestion_service_$i.log"
+    docker logs "like_filter_$i" >& "$LOGS_DIR/like_filter_$i.log"
+    docker logs "funny_filter_$i" >& "$LOGS_DIR/funny_filter_$i.log"
+    docker logs "day_grouper_$i" >& "$LOGS_DIR/day_grouper_$i.log"
+    docker logs "max_$i" >& "$LOGS_DIR/max.log"
+    docker logs "reporting_service_$i" >& "$LOGS_DIR/reporting_service_$i.log"
+done
