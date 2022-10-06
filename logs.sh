5840433d64b7968b4ae41a1290ddb87619d496ac
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-05 22:44:54 -0300

    Add trending to logs

--- logs.sh
+++ logs.sh
@@ -18,6 +18,7 @@ docker logs "reporting_service_1" >& "$LOGS_DIR/reporting_service_1.log"
 for i in $( seq 1 $INSTANCES )
 do
     docker logs "like_filter_$i" >& "$LOGS_DIR/like_filter_$i.log"
+    docker logs "trending_filter_$i" >& "$LOGS_DIR/trending_filter_$i.log"
     docker logs "funny_filter_$i" >& "$LOGS_DIR/funny_filter_$i.log"
     docker logs "day_grouper_$i" >& "$LOGS_DIR/day_grouper_$i.log"
 done
