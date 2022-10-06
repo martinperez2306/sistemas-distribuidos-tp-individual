214a83ea627fb8023f5000efd3acda491976aba0
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-05 21:34:25 -0300

    Add trending filter caller

---
+++ middleware/trending_filter_caller.py
@@ -0,0 +1,33 @@
+from dependencies.commons.constants import *
+from dependencies.commons.utils import json_to_video
+from dependencies.commons.message import Message
+from middleware.routing_caller import RoutingCaller
+
+
+class TrendingFilterCaller(RoutingCaller):
+    def __init__(self, config_params):
+        RoutingCaller.__init__(self, TRENDING_FILTER_EXCHANGE)
+        self.total_routes = int(config_params["service_instances"])
+    
+    def filter_by_trending(self, message: Message):
+        filter_by_trending_message = Message(MIDDLEWARE_MESSAGE_ID, message.request_id, message.source_id, message.operation_id, TRENDING_FILTER_GROUP_ID, message.body)
+        if START_PROCESS_OP_ID == message.operation_id:
+            self.connect()
+            self.__broadcast(filter_by_trending_message)
+        elif PROCESS_DATA_OP_ID == message.operation_id:
+            video = json_to_video(filter_by_trending_message.body)
+            video_id = video.id
+            routing_key = TRENDING_FILTER_GROUP_ID + "_" + str((hash(video_id) % self.total_routes) + 1)
+            self.publish_data(filter_by_trending_message.to_string(), str(routing_key))
+        elif LOAD_TOTAL_COUNTRIES == message.operation_id:
+            self.__broadcast(filter_by_trending_message)
+        elif END_PROCESS_OP_ID == message.operation_id:
+            self.__broadcast(filter_by_trending_message)
+            self.close()
+        else:
+            pass
+
+    def __broadcast(self, message):
+        for route in range(self.total_routes):
+            routing_key = routing_key = TRENDING_FILTER_GROUP_ID + "_" + str((route + 1))
+            self.publish_data(message.to_string(), str(routing_key))
