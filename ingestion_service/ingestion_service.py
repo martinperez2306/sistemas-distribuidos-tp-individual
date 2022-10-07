62809ac5afa4dd418718667f8cd3822b02132b1b
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-07 19:43:07 -0300

    Service instances in all services environment

--- ingestion_service/ingestion_service.py
+++ ingestion_service/ingestion_service.py
@@ -7,9 +7,7 @@ from dependencies.commons.work_service import WorkService
 
 class IngestionService(WorkService):
     def __init__(self, config_params):
-        id = config_params["service_id"]
-        group_id = config_params["group_id"]
-        super().__init__(id, group_id, INGESTION_SERVICE_QUEUE)
+        super().__init__(config_params, INGESTION_SERVICE_QUEUE)
         self.ingested_videos = list()
 
     def work(self, ch, method, properties, body):
@@ -21,17 +19,17 @@ class IngestionService(WorkService):
             video = Video.from_input(video_input)
             self.ingested_videos.append(video)
         elif END_PROCESS_OP_ID == ingestion_message.operation_id:
-            self.middleware_system_client.connect()
             self.__process_ingested_videos(ingestion_message)
-            self.middleware_system_client.call_filter_by_likes_and_trending(ingestion_message)
-            self.middleware_system_client.close()
+            self.middleware_system_client.call_filter_by_likes(ingestion_message)
+            self.middleware_system_client.call_filter_by_trending(ingestion_message)
             self.ingested_videos.clear()
 
     def __process_ingested_videos(self, ingestion_message: Message):
         for ingested_video in self.ingested_videos:
             ingested_video_message = Message(ingestion_message.id, ingestion_message.request_id, ingestion_message.source_id,
                                         PROCESS_DATA_OP_ID, ingestion_message.destination_id, to_json(ingested_video.__dict__))
-            self.middleware_system_client.call_filter_by_likes_and_trending(ingested_video_message)
+            self.middleware_system_client.call_filter_by_likes(ingested_video_message)
+            self.middleware_system_client.call_filter_by_trending(ingested_video_message)
         pass
         
     
