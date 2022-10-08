b7e50e82d71a44275ba3f9c8fa74e9e9fb073430
Martin Nicolas Perez <manperez@fi.uba.ar>
2022-10-08 17:43:46 -0300

    Update logs.sh

--- logs.sh
+++ logs.sh
@@ -10,15 +10,15 @@ INSTANCES=$1
 LOGS_DIR="logs"
 
 docker logs client >& "$LOGS_DIR/client.log"
-docker logs middleware >& "$LOGS_DIR/middleware.log"
-docker logs "ingestion_service_1" >& "$LOGS_DIR/ingestion_service_1.log"
-docker logs "max_1" >& "$LOGS_DIR/max_1.log"
-docker logs "reporting_service_1" >& "$LOGS_DIR/reporting_service_1.log"
 
 for i in $( seq 1 $INSTANCES )
 do
+    docker logs "ingestion_service_$i" >& "$LOGS_DIR/ingestion_service_$i.log"
     docker logs "like_filter_$i" >& "$LOGS_DIR/like_filter_$i.log"
     docker logs "trending_filter_$i" >& "$LOGS_DIR/trending_filter_$i.log"
     docker logs "funny_filter_$i" >& "$LOGS_DIR/funny_filter_$i.log"
     docker logs "day_grouper_$i" >& "$LOGS_DIR/day_grouper_$i.log"
 done
+
+docker logs "max" >& "$LOGS_DIR/max.log"
+docker logs "reporting_service_1" >& "$LOGS_DIR/reporting_service.log"
